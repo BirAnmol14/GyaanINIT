ee2dee581143fed39c10298134bd03c69f5b3d33
Bir Anmol Singh <f20180261@pilani.bits-pilani.ac.in>
2020-07-28 15:30:21 +0530

    create meet updated

--- client/src/components/create.jsx
+++ client/src/components/create.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
-
+import ServerRoutes from './ServerRoutes.js';
 
 function Create(){
+  async function logout(event){
+    let body=sessionStorage.getItem('gyaan-session');
+    alert(body);
+    const response=await fetch(ServerRoutes.logout,{
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: body
+    })
+    const status=await response.status;
+    if(status===200){
+      const res=await response.json();
+      if(res.status===true){
+        alert(res.message);
+        sessionStorage.removeItem('gyaan-session');
+        window.location.href='/';
+      }else{
+          alert(JSON.stringify(res));
+      }
+    }else{
+      alert('Error '+status);
+    }
+  }
   return(<div>
     <h1>create Meet</h1>
+    <button onClick={logout}>Logout</button>
     </div>);
 }
 
